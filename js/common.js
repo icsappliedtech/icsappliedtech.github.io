b9e5afb0f720f15b791160463ccfd8030780b14b
Jason Weimer <125435942+icsappliedtech@users.noreply.github.com>
2023-03-18 17:32:07 +0700

    Updates

--- js/common.js
+++ js/common.js
@@ -10,17 +10,17 @@ const sessionData = {
 const sessionCookie = document.cookie.split(';').find(cookie => cookie.startsWith('session='));
 if (!sessionCookie) {
   // Session cookie not found, redirect the user to the login page
-  window.location.href = 'login.html';
+  window.location.href = '/';
 } else {
   const sessionToken = sessionCookie.split('=')[1];
   try {
     const sessionData = JSON.parse(atob(sessionToken)); // Decode the session token and parse the JSON data
     if (!sessionData.isAuthenticated) {
       // User is not authenticated, redirect to the login page
-      window.location.href = 'login.html';
+      window.location.href = '/';
     }
   } catch (error) {
     // Invalid session token, redirect to the login page
-    window.location.href = 'login.html';
+    window.location.href = '/';
   }
 }
